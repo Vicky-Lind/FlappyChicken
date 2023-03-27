7a07474076e5f6029fd8f8fc446026ccf479731e
VictoriaLind <112470720+VictoriaLind@users.noreply.github.com>
2023-03-27 12:59:26 +0300

    Add background pictures from: https://opengameart.org/content/bevouliin-free-beach-game-background-for-game-developers

--- game.py
+++ game.py
@@ -1,26 +1,40 @@
 import pygame
 
+def main():
+    game = Game()
+    game.run()
+
 class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((800, 600))
+        self.running = False
 
     def run(self):
         self.clock = pygame.time.Clock()
         self.running = True
-        
+
         while self.running:
-            
-            self.screen.fill((255, 255, 255))
-            pygame.display.flip()
+            self.handle_events()
+            self.handle_game_logic()
+            self.update_screen()
 
             # Limit to 60fps
             self.clock.tick(60)
-    
+            
+        pygame.quit()
+
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-        
+    
+    def handle_game_logic(self):
+        pass
+
+    def update_screen(self):
+        self.screen.fill((230, 230, 255))
+        pygame.display.flip()
+
 if __name__ == "__main__":
     main()
